19ad9561d0f3c9fcc5aa4e03df1e9cce1b079d85
Chritie MPASSI <christmp19@gmail.com>
2024-04-24 15:34:57 +0100

    Fixed bug

--- src/Requests.js
+++ src/Requests.js
@@ -4,7 +4,7 @@ const API_KEY = "f81980ff410e46f422d64ddf3a56dddd";
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}& language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}& with_networks=213`,
-    fetchTopRated: `/ movie/top_rated?api_key=${API_KEY}& language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}& language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}& with_genres=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}& with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}& with_genres=27`,
