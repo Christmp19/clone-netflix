ac6cb37cad142d8645740de41976404164e9db36
Chritie MPASSI <christmp19@gmail.com>
2024-04-25 12:29:11 +0100

    Authentificator (register and login)

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDDlx_ZDEn-7McjbyfVY4Eauux8X4Pwods",
     authDomain: "clone-netflix-bf201.firebaseapp.com",
@@ -6,3 +11,11 @@ const firebaseConfig = {
     messagingSenderId: "457716720660",
     appId: "1:457716720660:web:0f4d60a8f9123385c27301"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export { auth };
+export default db;
--- src/screens/LoginSreen.js
+++ src/screens/LoginSreen.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginScreen.css';
+import SignupScreen from './SignupScreen';
 
 function LoginSreen() {
+    const [signIn, setSignIn] = useState(false);
     return (
         <div className='loginScreen'>
-          
+            <div className='loginScreen_background'>
+                <img className='loginScreen_logo' src="/logo.svg" alt="Logo de Netflix" />
+                <button onClick={() => setSignIn(true)} className='loginScreen_button'>Sign In</button>
+                <div className='loginScreen_gradient' />
+            </div>
+            <div className='loginScreen_body'>
+                {signIn ? (
+                    <SignupScreen />
+                ) : (
+                    <>
+                        <h1>Unlimited films, TV programmes and more.</h1>
+                        <h2>Watch anywhere. Cancel at any time.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+                        <div className='loginScreen_input'>
+                            <form>
+                                <input type='email' placeholder='Email Address' />
+                                <button onClick={() => setSignIn(true)} className='loginScreen_getStarted'>GET STARTED</button>
+                            </form>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     )
 }
