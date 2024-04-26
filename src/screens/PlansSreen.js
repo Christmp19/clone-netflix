6ad5725cd65e9fb4b312a45621821e2dde6c1982
Chritie MPASSI <christmp19@gmail.com>
2024-04-26 14:16:21 +0100

    Building the PlanScreen Component (2/3),  Testing the Stripe Checkout & Subscription Flow

--- src/screens/PlansSreen.js
+++ src/screens/PlansSreen.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import './PlansSreen.css';
 import db from '../firebase';
+import { selectUser } from '../features/userSlice';
+import { useSelector } from 'react-redux';
+import { loadStripe } from '@stripe/stripe-js';
+
 
 function PlansSreen() {
     const [products, setProducts] = useState([]);
+    const user = useSelector(selectUser);
 
     useEffect(() => {
         db.collection('products').where('active', '==', true)
@@ -24,6 +29,37 @@ function PlansSreen() {
     }, []);
 
     console.log(products);
+
+    const loadCheckout = async (priceId) => {
+        const docRef = await db
+            .collection('customers')
+            .doc(user.uid)
+            .collection('checkout_sessions')
+            .add({
+                price: priceId,
+                success_url: window.location.origin,
+                cancel_url: window.location.origin,
+            });
+
+        docRef.onSnapshot(async (snap) => {
+            const { error, sessionId } = snap.data();
+
+            if (error) {
+                // Show an error to your customer and
+                // inspect your cloud function logs in the firebase console.
+                alert(`An error occured: ${error.message}`);
+            }
+
+            if (sessionId) {
+                // We have a sessions, let's rerirect to Checkout
+                // Init Stripe
+                const stripe = await loadStripe('pk_test_51P9TBC00eGP3dNCUstPZR8J0rhustmY23Thqt0pdr3AxlLPcgmFhCQbbiEokoRDSDBVBaWNA8cQDwI43ENJ32UnJ00OqP4rudm');
+                stripe.redirectToCheckout({ sessionId });
+            }
+
+        })
+    };
+
     return (
         <div className='plansSreen'>
             {Object.entries(products).map(([productId, productData]) => {
@@ -33,9 +69,11 @@ function PlansSreen() {
                         <div className='plansSreen_info'>
                             <h5>{productData.name}</h5>
                             <h6>{productData.description}</h6>
-                        </div> 
-                        <button>Subscribe</button>
-                    </div> 
+                        </div>
+                        <button onClick={() => loadCheckout(productData.prices.priceId)}>
+                            Subscribe
+                        </button>
+                    </div>
                 );
             })}
 
